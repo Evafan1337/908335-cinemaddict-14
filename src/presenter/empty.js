abcf3f25ef72cfac53643f49bcef84a588cca0f1
Sasha <as007ershov@gmail.com>
2021-04-26 23:49:43 +0400

    Создание презентера для пустого списка фильмов

---
+++ src/presenter/empty.js
@@ -0,0 +1,21 @@
+import EmptyFilms from '../view/empty-films';
+import {render, RenderPosition} from '../utils';
+
+export default class EmptyPresenter {
+  constructor(emptyContainer) {
+    this._emptyContainer = emptyContainer;
+    this._emptyFilms = null;
+  }
+
+  /**
+   * Метод инициализации
+   */
+  init() {
+    this._emptyFilms = new EmptyFilms();
+    this._renderEmpty();
+  }
+
+  _renderEmpty() {
+    render(this._emptyContainer, this._emptyFilms.getElement(), RenderPosition.BEFOREEND);
+  }
+}
