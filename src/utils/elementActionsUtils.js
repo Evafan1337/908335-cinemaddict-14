81fc0d8faf8879256d586fc5f0097f3b6a9772bb
Sasha <as007ershov@gmail.com>
2021-05-04 23:45:11 +0400

    Переработка утилит

---
+++ src/utils/elementActionsUtils.js
@@ -0,0 +1,60 @@
+import AbstractView from '../view/abstract';
+
+/**
+ * Функция создания HTML элемента
+ * @param {string} template - HTML элемент в формате строки, который будет вложен в container
+ * @param {Object} newElement.firstChild - созданный HTML элемент
+ */
+export const createElement = (template) => {
+  const newElement = document.createElement('div'); // 1
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+/**
+ * Функция обновления элемента массива
+ * @param {Array} items - массив данных
+ * @return {Object} update - элемент для замены
+ */
+export const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1),
+  ];
+};
+
+/**
+ * Функция замены элемента с помощью replaceChild
+ * @param {Object} newChild - новый элемент
+ * @return {Object} oldChild - старый элемент
+ */
+export const replace = (newChild, oldChild) => {
+  if (oldChild instanceof AbstractView) {
+    oldChild = oldChild.getElement();
+  }
+
+  if (newChild instanceof AbstractView) {
+    newChild = newChild.getElement();
+  }
+
+  const parent = oldChild.parentElement;
+
+  if (parent === null || oldChild === null || newChild === null) {
+    return;
+  }
+
+  parent.replaceChild(newChild, oldChild);
+};
+
+export const remove = (component) => {
+  component.getElement().remove();
+  component.removeElement();
+};
---
+++ src/utils/renderUtils.js
@@ -0,0 +1,32 @@
+export const RenderPosition = {
+  AFTERBEGIN: 'afterbegin',
+  BEFOREEND: 'beforeend',
+};
+
+/**
+ * Функция рендера компонента
+ * @param {object} container - HTML элемент в который будет "вложен" элемент template
+ * @param {string} template - HTML элемент в формате строки, который будет вложен в container
+ * @param {string} place - аргумент для insertAdjacentHTML (параметр вставки)
+ */
+export const render = (container, element, place = 'beforeend') => {
+  element = element.getElement();
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+/**
+ * Функция рендера шаблона
+ * @param {object} container - HTML элемент в который будет "вложен" элемент template
+ * @param {string} template - HTML элемент в формате строки, который будет вложен в container
+ * @param {string} place - аргумент для insertAdjacentHTML (параметр вставки)
+ */
+export const renderTemplate = (container, template, place = 'beforeend') => {
+  container.insertAdjacentHTML(place, template);
+};
